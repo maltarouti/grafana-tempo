d9278c5a54aa424caa441a03e3003ab23bd70c21
Murtada <murtada.altarouti@gmail.com>
2024-10-24 06:30:13 +0300

    jaeger message

--- publish_to_kafka.py
+++ publish_to_kafka.py
@@ -1,20 +1,16 @@
 from typing import List
-
 import json
 import time
 import random
 from uuid import uuid4
 from dataclasses import dataclass
-
-
 from kafka import KafkaProducer
-
 
 SERVER = "localhost:9094"
 TOPIC = "tempo"
 
-trace_id = str(uuid4()).replace("-", "")
-root_span_id = str(uuid4()).replace("-", "")[:16]
+trace_id = "AAAAAAAAAAAAAAAAAAAAER=="
+root_span_id = "AAAAAAAAAAM="
 
 
 @dataclass
@@ -25,65 +21,29 @@ class Span:
     end_time: int
 
 
-def get_spans() -> List[dict]:
+def get_spans() -> List[Span]:
     start_time = int(time.time() * 1e9)
-    end_time = int(time.time() * 1e9)
+    end_time = start_time
     spans = []
 
     for _ in range(random.randint(0, 5)):
         span_id = str(uuid4()).replace("-", "")[:16]
-        name = uuid4().split("-")[0]
-        span_start_time = int(time.time() * 1e9)
-        end_time += random.randint(0, 7)
+        name = str(uuid4()).split("-")[0]
+        span_start_time = end_time
+        span_end_time = span_start_time + random.randint(0, 7) * int(1e9)
         spans.append(Span(span_id=span_id,
                           name=name,
                           start_time=span_start_time,
-                          end_time=end_time))
+                          end_time=span_end_time))
+        end_time = span_end_time
 
-    # root
-    name = uuid4().split("-")[0]
-    span_start_time = int(time.time() * 1e9)
+    name = str(uuid4()).split("-")[0]
     spans.append(Span(span_id=root_span_id,
                       name=name,
-                      start_time=span_start_time,
+                      start_time=start_time,
                       end_time=end_time))
 
     return spans
-
-    return {
-        "resource": {
-            "attributes": [
-                {
-                    "key": "service.name",
-                    "value": {
-                        "stringValue": "GET"
-                    }
-                }
-            ]
-        },
-        "scopeSpans": [
-            {
-                "spans": [
-                    {
-                        "traceId": trace_id,
-                        "spanId": root_span_id,
-                        "name": "GET",
-                        "startTimeUnixNano": start_time,
-                        "endTimeUnixNano": end_time,
-                        "kind": 1,
-                        "attributes": [
-                            {
-                                "key": "systemCode",
-                                "value": {
-                                    "stringValue": "AAA"
-                                }
-                            }
-                        ]
-                    }
-                ]
-            }
-        ]
-    }
 
 
 def publish() -> None:
@@ -92,12 +52,30 @@ def publish() -> None:
         value_serializer=lambda v: json.dumps(v).encode('utf-8')
     )
 
-    for i in range(10):
-        message = {'message_id': i, 'content': f'This is message {i}'}
-        producer.send(TOPIC, value=message)
-        print(f'Sent: {message}')
-        time.sleep(1)
+    spans = get_spans()
+    spans, root = spans[:-1], spans[-1]
 
+    root_data = {
+        "traceId": trace_id,
+        "spanId": root_span_id,
+        "operationName": "example-operation-1",
+        "references": [],
+        "startTime": root.start_time,
+        "duration": "10ns",
+        "tags": [],
+        "process": {
+            "serviceName": "example-service-1",
+            "tags": []
+        },
+        "logs": [
+            {
+                "timestamp": root.start_time,
+                "fields": []
+            },
+        ]
+    }
+
+    producer.send(TOPIC, value=root_data)
     producer.close()
 
 
